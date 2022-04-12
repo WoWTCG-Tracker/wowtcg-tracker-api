238716c6443b5fe5b47bd2cb8a701f70210d2bc6
Karel Bašta <bastakka@email.cz>
2022-04-12 16:43:23 +0200

    Format to black

--- api/__init__.py
+++ api/__init__.py
@@ -5,13 +5,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 app = flask.Flask(__name__)
-if os.getenv('FLASK_ENV') == 'development':
-    app.config['DEBUG'] = True
+
+if os.getenv("FLASK_ENV") == "development":
+    app.config["DEBUG"] = True
+
 
 @app.route("/", methods=["GET"])
 def api_root():
     """API root"""
     return "Hello world!"
+
 
 if __name__ == "__main__":
     app.run()
--- tests/test_api.py
+++ tests/test_api.py
@@ -3,15 +3,18 @@ import unittest
 
 from api import api_root
 
+
 class TestRoot(unittest.TestCase):
     """
     Test the root of the API
-    
+
     Root is set to return "Hello world!" string.
     """
+
     def test_root(self):
         """Test the root of the API"""
         self.assertEqual(api_root(), "Hello world!")
+
 
 if __name__ == "__main__":
     unittest.main()
