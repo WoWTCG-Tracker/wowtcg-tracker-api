a40992e373f49e3592c8c437f4a08d5993b78aae
Karel Bašta <bastakka@email.cz>
2022-04-12 15:17:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@ setup(
     version='0.1',
     description='A simple API for WoWTCG cards',
     license='GNU General Public License v3.0',
+    license_file='LICENSE',
     author='Karel Bašta',
     author_email='bastakka@email.cz',
-    url='wowtcg.bastakka.eu',
+    url='https://github.com/bastakka/wowtcg-tracker',
+    platforms=['any'],
     packages=['api'],
     include_package_data=True,
     install_requires=[
