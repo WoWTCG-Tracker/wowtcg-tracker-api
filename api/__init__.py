de54b4c0e4e207feb634c7b8e8af249c4978aa1e
Karel Bašta <bastakka@email.cz>
2022-04-27 21:39:17 +0200

    Add metadata to FastAPI app
    Also rename "read" to "get" to match the HTTP naming

--- api/__init__.py
+++ api/__init__.py
@@ -1,17 +1,16 @@
 """API server for WoWTCG Tracker"""
-import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 
 load_dotenv()
-app = FastAPI()
+app = FastAPI(title="WoWTCG Tracker API", description="API for WoWTCG Tracker")
 
 
 @app.get("/")
-async def read_root():
-    """
+async def get_root():
+  """
     Root of the API
 
     Root is set to return "Hello world!" string.
     """
-    return "Hello world!"
+  return "Hello world!"
